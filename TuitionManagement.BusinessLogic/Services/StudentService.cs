688f660d809f232c92d73985890cef783d8b3c9b
Velraaj <velraaj30@gmail.com>
2026-06-12 03:57:51 +0530

    Added Identity package to Application and data project

--- TuitionManagement.BusinessLogic/Services/StudentService.cs
+++ TuitionManagement.BusinessLogic/Services/StudentService.cs
@@ -15,27 +15,20 @@ namespace TuitionManagement.BusinessLogic.Services
 
         public async Task<APIResponse<List<StudentResponse>>> GetAllStudents()
         {
-            try
+            var result = await _db.Students.Select(x => new StudentResponse
             {
-                var result = await _db.Students.Select(x => new StudentResponse
-                {
-                    StudentUID = x.StudentUID,
-                    StudentName = x.StudentName,
-                    PhoneNumber = x.PhoneNumber,
-                    EmailAddress = x.EmailAddress,
-                    DateCreated = x.DateCreated,
-                    DateModified = x.DateModified,
-                    CreatedBy = x.CreatedBy,
-                    ModifiedBy = x.ModifiedBy,
-                    BatchCount = x.StudentBatches.Count()
-                }).ToListAsync();
+                StudentUID = x.StudentUID,
+                StudentName = x.StudentName,
+                PhoneNumber = x.PhoneNumber,
+                EmailAddress = x.EmailAddress,
+                DateCreated = x.DateCreated,
+                DateModified = x.DateModified,
+                CreatedBy = x.CreatedBy,
+                ModifiedBy = x.ModifiedBy,
+                BatchCount = x.StudentBatches.Count()
+            }).ToListAsync();
 
-                return new APIResponse<List<StudentResponse>>(){StatusCode= APIStatusCodes.Success, Data = result, Message = "Student records retrieved successfully"};
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            return new APIResponse<List<StudentResponse>>() { StatusCode = APIStatusCodes.Success, Data = result, Message = "Student records retrieved successfully" };
         }
     }
 }
